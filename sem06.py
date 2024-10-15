27f7b13b83f0afb65e49cb638cfef5d84ab74f3f
DanielERomero <i2422512@continental.edu.pe>
2024-10-14 21:25:41 -0500

    ejecutando ejercicio1

--- sem06.py
+++ sem06.py
@@ -6,5 +6,5 @@ st.title("Ejercicios con bucles basicos en python")
 #Ejercicio 1: Imprimir 10 veces 'hola mundo'
 st.subheader("Ejercicio 1 Imprimir 'Hola Mundo' 10 veces")
 if st.button("Ejecutar Ejercicio 1")
-    for i in range(10):
+    for i in range(10): #range genera una secuencia de 0 al 9, for hace bucle
         st.write("Hola Mundo")
