48d7c223546ee1d259cc52423000bc95ebe44779
DanielERomero <i2422512@continental.edu.pe>
2024-10-14 21:49:43 -0500

    corrigiendo num

--- sem06.py
+++ sem06.py
@@ -15,8 +15,8 @@ if st.button("ejecutar Ejercicio 2"):
     for i in range(1, 11):
         st.write(i)
 #Ejercicio: 3 Tabla de multiplicar
-st.subheader("ejercicio 3: Imprimir la tabla de multiplicar del numero ingresado")
-nun = st.number_input("Ingrese un número para ver su tabla de multiplicar del 1 al 12", min_value=1)
+st.subheader("Ejercicio 3: Imprimir la tabla de multiplicar del numero ingresado")
+num = st.number_input("Ingrese un número para ver su tabla de multiplicar del 1 al 12", min_value=1)
 if st.button("Ejecutar Ejercicio 3"):
     for i in range(1, 13):
         st.write(f"{num} x {i} = {num * i}")
