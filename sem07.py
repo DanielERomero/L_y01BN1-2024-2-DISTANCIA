d7b18d3d9112dc716e008723ae90727fbfb9305c
DanielERomero <i2422512@continental.edu.pe>
2024-10-21 21:27:36 -0500

    menu seleccion

---
+++ sem07.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+def mostrar_menu():
+    st.title("Ejemplo de Menú")
+    st.write("Selecciona una opcion del menú")
+
+    menu = ["archivo","Editar","Ver","Salir"]
+    seleccion = ""
+
+    
+    seleccion = st.radio("Menú", menu)
+
+    if seleccion == "Archivo":
+        st.write("seleccionaste:archivo")
+     elif seleccion == "Editar":
+         st.write("Seleccionaste: Editar")
+    elif seleccion == "Salir":
+         st.write("¡Saliendo del menú")
+            
+if _name_ == "_main_":
+    mostrar_menu()                    
