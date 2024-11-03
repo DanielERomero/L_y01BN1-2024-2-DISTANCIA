507f158e4d7d65dcf34053651cb9890b0911e6bc
DanielERomero <i2422512@continental.edu.pe>
2024-11-03 08:18:20 -0500

    agfegando espacio en == 0

--- ejercicio01_idl3.py
+++ ejercicio01_idl3.py
@@ -5,7 +5,7 @@ st.title("Ejercicio IDL3")
 #Ejercicio 1: calcular y mostrar la suma de los número pares comprendidos entre 20 y 400
 st.subheader("Ejercicio1: Suma de numero pares")
 if st.button("Ejecutar ejercicio 1: "):
-    suma_pares = sum(num for num in rage (2, 401) if num % ==0)
+    suma_pares = sum(num for num in rage (2, 401) if num % == 0)
     #mostrar el resultado:
     st.write(f"La suma de los numeros pares entre 20 y 400 es: {suma_pares}")
 
