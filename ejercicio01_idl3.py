79ecab9d725efa8a65985a6b50b65515bd601aaf
DanielERomero <i2422512@continental.edu.pe>
2024-11-03 08:17:55 -0500

    corrigiendo titulo

--- ejercicio01_idl3.py
+++ ejercicio01_idl3.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title Ejercicio IDL3
+st.title("Ejercicio IDL3")
 
 #Ejercicio 1: calcular y mostrar la suma de los número pares comprendidos entre 20 y 400
 st.subheader("Ejercicio1: Suma de numero pares")
