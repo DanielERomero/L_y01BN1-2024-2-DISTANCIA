8346905922405ae3784e8847ecb75adb6d388c9d
DanielERomero <i2422512@continental.edu.pe>
2024-11-03 08:50:19 -0500

    ejercicio 03

---
+++ ejercicio04_idl3.py
@@ -0,0 +1,21 @@
+#ejercicio4 crear un programa que pida al usuario una contraseña de forma repetitiva minetras no introduzca "asdasd"
+import streamlit as st
+# Definir la contraseña correcta
+CONTRASEÑA_CORRECTA = "asdasd"
+
+def verificar_contraseña(contraseña):
+    if contraseña == CONTRASEÑA_CORRECTA:
+        return True
+    else:
+        return False
+st.title("Ingreso de Contraseña")
+
+contraseña = st.text_input("Introduce la contraseña:", type="password")
+
+if contraseña:
+    if verificar_contraseña(contraseña):
+        st.write("Bienvenido")
+    else:
+        st.write("Contraseña incorrecta. Inténtalo de nuevo.")
+if __name__ == "__main__":
+    verificar_contraseña()
